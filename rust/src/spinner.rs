cbed7518df35e917c0f5ad79c869668abdf8713d
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2017-03-24 00:44:36 +0100

    Reworked Spinner code to use the new version of messages (#4 and #6)

--- rust/src/spinner.rs
+++ rust/src/spinner.rs
@@ -3,19 +3,142 @@ use mouros_rust_bindings::mailbox::{Mailbox, MailboxRaw};
 use mouros_rust_bindings::CVoid;
 use mouros_rust_bindings::tasks;
 
-use bindings::messages;
 use bindings::constants;
 use bindings::message_dispatcher;
 
 use core::ptr;
+use core::convert::TryFrom;
+
+use bindings::message_dispatcher::{MessageWrapper, MemManagement};
+
+mod MsgTypes {
+    pub const SET_PLAN: u32 = 0;
+    pub const GET_PLAN: u32 = 1;
+    pub const PLAN_REPLY: u32 = 2;
+    pub const SET_STATE: u32 = 3;
+    pub const GET_STATE: u32 = 4;
+    pub const STATE_REPLY: u32 = 5;
+    pub const RET_VAL: u32 = 6;
+}
+
+
+
+#[repr(C)]
+pub struct spin_plan_data {
+    pub channel_num: u8,
+    pub spin_plan_leg_count: u32,
+    pub plan_legs: [pwm_leg; 10usize],
+}
+
+#[repr(C)]
+pub struct pwm_leg {
+    pub duration_msecs: u32,
+    pub target_pct: f32,
+}
+
+#[repr(C)]
+pub struct spin_channel {
+    pub channel_num: u8,
+}
+
+#[repr(C)]
+pub struct ret_val {
+    pub ret_val: i32,
+}
+
+mod SpinStates {
+    pub const STOPPED: u32 = 0;
+    pub const RUNNING: u32 = 1;
+    pub const SPINNING_DOWN: u32 = 2;
+}
+
+#[repr(C)]
+pub struct spin_state_set_data {
+    pub channel_num: u8,
+    pub state: u32,
+}
+
+#[repr(C)]
+pub struct spin_state_data {
+    pub channel_num: u8,
+    pub state: u32,
+    pub plan_time_elapsed_msecs: u32,
+    pub output_val_pct: f32,
+}
+
+
+pub enum Message<'a> {
+    SetSpinPlan(&'a mut spin_plan_data),
+    GetSpinPlan(&'a mut spin_channel),
+    SpinPlanReply(&'a mut spin_plan_data),
+    SetSpinState(&'a mut spin_state_set_data),
+    GetSpinState(&'a mut spin_channel),
+    SpinStateReply(&'a mut spin_state_data),
+    ReturnValue(&'a mut ret_val),
+}
+
+
+impl<'a> message_dispatcher::MemManagement for Message<'a> {
+    fn alloc(msg_type: u32) -> *mut message_dispatcher::message {
+        unsafe { get_alloc_fn()(msg_type) }
+    }
+
+    fn free(msg_ptr: *mut message_dispatcher::message) {
+        unsafe { get_free_fn()(msg_ptr) }
+    }
+}
+
+
+
+impl<'a> TryFrom<*mut message_dispatcher::message> for Message<'a> {
+    type Err = ();
+
+    fn try_from(raw_msg_ptr: *mut message_dispatcher::message) -> Result<Self, Self::Err> {
+        unsafe {
+            if raw_msg_ptr.is_null() || (*raw_msg_ptr).data.is_null() {
+                return Err(());
+            }
+
+            match (*raw_msg_ptr).msg_type {
+                MsgTypes::SET_PLAN => {
+                    Ok(Message::SetSpinPlan(&mut *((*raw_msg_ptr).data as *mut spin_plan_data)))
+                }
+                MsgTypes::GET_PLAN => {
+                    Ok(Message::GetSpinPlan(&mut *((*raw_msg_ptr).data as *mut spin_channel)))
+                }
+                MsgTypes::PLAN_REPLY => {
+                    Ok(Message::SpinPlanReply(&mut *((*raw_msg_ptr).data as *mut spin_plan_data)))
+                }
+                MsgTypes::SET_STATE => {
+                    Ok(Message::SetSpinState(&mut *((*raw_msg_ptr).data as
+                                                    *mut spin_state_set_data)))
+                }
+                MsgTypes::GET_STATE => {
+                    Ok(Message::GetSpinState(&mut *((*raw_msg_ptr).data as *mut spin_channel)))
+                }
+                MsgTypes::STATE_REPLY => {
+                    Ok(Message::SpinStateReply(&mut *((*raw_msg_ptr).data as *mut spin_state_data)))
+                }
+                MsgTypes::RET_VAL => {
+                    Ok(Message::ReturnValue(&mut *((*raw_msg_ptr).data as *mut ret_val)))
+                }
+                _ => {
+                    Self::free(raw_msg_ptr);
+                    Err(())
+                }
+            }
+        }
+    }
+}
+
 
 const NUM_CHANNELS: usize = 1;
 
 
 enum SpinnerEvent<'a> {
-    SetPlan(&'a messages::spin_plan_data),
+    SetPlan(&'a spin_plan_data),
     GetPlan,
-    SetState(&'a messages::spin_state_set_data),
+    SetState(&'a spin_state_set_data),
     GetState,
 }
 
@@ -110,14 +233,13 @@ impl Channel {
     }
 
     fn send_spin_plan_reply(&self, trans_id: u32) {
-        let mut msg = if let Ok(msg) =
-            messages::MessageWrapper::new(trans_id, messages::MsgTypes::SPIN_PLAN_REPLY) {
+        let mut msg = if let Ok(msg) = MessageWrapper::new(trans_id, MsgTypes::PLAN_REPLY) {
             msg
         } else {
             return;
         };
 
-        if let messages::Message::SpinPlanReply(ref mut data) = *msg {
+        if let Message::SpinPlanReply(ref mut data) = *msg {
             data.channel_num = self.id;
             data.spin_plan_leg_count = self.plan_len as u32;
             for leg_idx in 0..self.plan_len {
@@ -128,23 +250,22 @@ impl Channel {
             return;
         }
 
-        message_dispatcher::send_message(msg);
+        get_outgoing_queue().write(msg.into());
     }
 
     fn send_spin_state_reply(&self, trans_id: u32) {
-        let mut msg = if let Ok(msg) =
-            messages::MessageWrapper::new(trans_id, messages::MsgTypes::SPIN_STATE_REPLY) {
+        let mut msg = if let Ok(msg) = MessageWrapper::new(trans_id, MsgTypes::STATE_REPLY) {
             msg
         } else {
             return;
         };
 
-        if let messages::Message::SpinStateReply(ref mut data) = *msg {
+        if let Message::SpinStateReply(ref mut data) = *msg {
             data.channel_num = self.id;
             data.state = match self.state {
-                ChannelState::Stopped => messages::SpinStates::STOPPED,
-                ChannelState::Running => messages::SpinStates::RUNNING,
-                ChannelState::Spindown => messages::SpinStates::SPINNING_DOWN,
+                ChannelState::Stopped => SpinStates::STOPPED,
+                ChannelState::Running => SpinStates::RUNNING,
+                ChannelState::Spindown => SpinStates::SPINNING_DOWN,
             };
             data.plan_time_elapsed_msecs = self.elapsed_time_msecs;
             data.output_val_pct = self.output_val_pct;
@@ -153,43 +274,42 @@ impl Channel {
             return;
         }
 
-        message_dispatcher::send_message(msg);
+        get_outgoing_queue().write(msg.into());
     }
 
     fn send_return_val(&self, trans_id: u32, ret_val: i32) {
-        let mut msg = if let Ok(msg) = messages::MessageWrapper::new(trans_id,
-                                                                     messages::MsgTypes::RET_VAL) {
+        let mut msg = if let Ok(msg) = MessageWrapper::new(trans_id, MsgTypes::RET_VAL) {
             msg
         } else {
             return;
         };
 
-        if let messages::Message::ReturnValue(ref mut data) = *msg {
+        if let Message::ReturnValue(ref mut data) = *msg {
             data.ret_val = ret_val;
         } else {
             return;
         }
 
-        message_dispatcher::send_message(msg);
+        get_outgoing_queue().write(msg.into());
     }
 
-    fn set_channel_state(&mut self, data: &messages::spin_state_set_data) {
+    fn set_channel_state(&mut self, data: &spin_state_set_data) {
         critical!({
-            if self.state == ChannelState::Running && data.state == messages::SpinStates::STOPPED {
-                self.state = ChannelState::Spindown;
-            } else if self.state == ChannelState::Stopped &&
-                      data.state == messages::SpinStates::RUNNING {
+                      if self.state == ChannelState::Running && data.state == SpinStates::STOPPED {
+                          self.state = ChannelState::Spindown;
+                      } else if self.state == ChannelState::Stopped &&
+                      data.state == SpinStates::RUNNING {
                 self.state = ChannelState::Running;
             }
-        });
+                  });
     }
 
-    fn set_channel_plan(&mut self, data: &messages::spin_plan_data) {
+    fn set_channel_plan(&mut self, data: &spin_plan_data) {
         critical!({
-            if self.state != ChannelState::Stopped {
-                return;
-            }
-        });
+                      if self.state != ChannelState::Stopped {
+                          return;
+                      }
+                  });
 
         self.plan_len = data.spin_plan_leg_count as usize;
         for leg_idx in 0..self.plan_len {
@@ -202,75 +322,123 @@ impl Channel {
 
 
 
-pub struct SpinnerContext {
+struct SpinnerContext {
     channels: [Channel; NUM_CHANNELS],
-    msg_buf: Mailbox<*mut messages::message>,
+    subsystem_conf: &'static message_dispatcher::subsystem_message_conf,
 }
 
-static mut MSG_BUF_MEM: [*mut messages::message; 30] = [0 as *mut messages::message; 30];
-static mut SPINNER_CONTEXT: Option<SpinnerContext> = None;
 
+static mut SPINNER_CTX: Option<SpinnerContext> = None;
 
-#[no_mangle]
-pub static mut SPINNER_MSG_QUEUE_PTR: *const MailboxRaw = 0 as *const MailboxRaw;
-
-#[no_mangle]
-pub extern "C" fn spinner_get_context() -> *mut CVoid {
+fn get_channels() -> &'static mut [Channel] {
     unsafe {
-        if SPINNER_CONTEXT.is_none() {
-            let mut ctx = SpinnerContext {
-                channels: [Channel::default(); NUM_CHANNELS],
-                msg_buf: Mailbox::new(&mut MSG_BUF_MEM),
-            };
+        match SPINNER_CTX {
+            Some(ref mut ctx) => &mut ctx.channels,
+            None => panic!(),
+        }
+    }
+}
 
-            for (id, ch) in ctx.channels.iter_mut().enumerate() {
-                ch.id = id as u8;
-            }
+fn get_incoming_queue() -> &'static mut Mailbox<*mut message_dispatcher::message> {
+    unsafe {
+        match SPINNER_CTX {
+            Some(ref ctx) => &mut *ctx.subsystem_conf.incoming_msg_queue,
+            None => panic!(),
+        }
+    }
+}
 
-            SPINNER_CONTEXT = Some(ctx);
-            SPINNER_MSG_QUEUE_PTR = SPINNER_CONTEXT.as_mut().unwrap().msg_buf.get_raw_mb_struct();
+fn get_outgoing_queue() -> &'static mut Mailbox<*mut message_dispatcher::message> {
+    unsafe {
+        match SPINNER_CTX {
+            Some(ref ctx) => &mut *ctx.subsystem_conf.outgoing_msg_queue,
+            None => panic!(),
+        }
+    }
+}
+
+fn get_outgoing_error_queue() -> &'static mut Mailbox<i32> {
+    unsafe {
+        match SPINNER_CTX {
+            Some(ref ctx) => &mut *ctx.subsystem_conf.outgoing_err_queue,
+            None => panic!(),
+        }
+    }
+}
+
+fn get_alloc_fn() -> unsafe extern "C" fn(msg_type_id: u32) -> *mut message_dispatcher::message {
+    unsafe {
+        match SPINNER_CTX {
+            Some(ref ctx) => ctx.subsystem_conf.alloc_message.unwrap(),
+            None => panic!(),
+        }
+    }
+}
+
+fn get_free_fn() -> unsafe extern "C" fn(msg: *mut message_dispatcher::message) {
+    unsafe {
+        match SPINNER_CTX {
+            Some(ref ctx) => ctx.subsystem_conf.free_message.unwrap(),
+            None => panic!(),
+        }
+    }
+}
+
+
+
+
+#[no_mangle]
+pub extern "C" fn spinner_rust_init(conf: *mut message_dispatcher::subsystem_message_conf) {
+    unsafe {
+        if conf.is_null() || (*conf).incoming_msg_queue.is_null() ||
+           (*conf).outgoing_msg_queue.is_null() || (*conf).outgoing_err_queue.is_null() ||
+           (*conf).alloc_message.is_some() || (*conf).free_message.is_some() {
+            panic!();
         }
 
-        SPINNER_CONTEXT.as_mut().unwrap() as *mut SpinnerContext as *mut CVoid
+        SPINNER_CTX = Some(SpinnerContext {
+                               channels: [Channel::default(); NUM_CHANNELS],
+                               subsystem_conf: &*conf,
+                           });
     }
 }
 
 #[no_mangle]
-pub extern "C" fn spinner_comm_loop(context_ptr: *mut CVoid) {
+pub extern "C" fn spinner_comm_loop(_params: *mut CVoid) {
 
-    let context: &mut SpinnerContext = unsafe {
-        if context_ptr != ptr::null_mut() {
-            &mut *(context_ptr as *mut SpinnerContext)
-        } else {
-            panic!();
-        }
-    };
+    let channels = get_channels();
+    let in_queue = get_incoming_queue();
 
-    if let Some(msg_ptr) = context.msg_buf.read() {
-        if let Ok((trans_id, msg)) = messages::MessageWrapper::from_raw(msg_ptr) {
+    if let Some(msg_ptr) = in_queue.read() {
+        if let Ok(msg) = MessageWrapper::try_from(msg_ptr) {
+
             match *msg {
-                messages::Message::SetSpinPlan(ref data) => {
+                Message::SetSpinPlan(ref data) => {
                     let ch_num = data.channel_num as usize;
                     if ch_num < NUM_CHANNELS {
-                        context.channels[ch_num].send_event(trans_id, SpinnerEvent::SetPlan(data));
+                        channels[ch_num].send_event(msg.get_transaction_id(),
+                                                    SpinnerEvent::SetPlan(data));
                     }
                 }
-                messages::Message::GetSpinPlan(ref data) => {
+                Message::GetSpinPlan(ref data) => {
                     let ch_num = data.channel_num as usize;
                     if ch_num < NUM_CHANNELS {
-                        context.channels[ch_num].send_event(trans_id, SpinnerEvent::GetPlan);
+                        channels[ch_num].send_event(msg.get_transaction_id(),
+                                                    SpinnerEvent::GetPlan);
                     }
                 }
-                messages::Message::SetSpinState(ref data) => {
+                Message::SetSpinState(ref data) => {
                     let ch_num = data.channel_num as usize;
                     if ch_num < NUM_CHANNELS {
-                        context.channels[ch_num].send_event(trans_id, SpinnerEvent::SetState(data));
+                        channels[ch_num].send_event(msg.get_transaction_id(),
+                                                    SpinnerEvent::SetState(data));
                     }
                 }
-                messages::Message::GetSpinState(ref data) => {
+                Message::GetSpinState(ref data) => {
                     let ch_num = data.channel_num as usize;
                     if ch_num < NUM_CHANNELS {
-                        context.channels[ch_num].send_event(trans_id, SpinnerEvent::GetState);
+                        channels[ch_num].send_event(msg.get_transaction_id(),
+                                                    SpinnerEvent::GetState);
                     }
                 }
                 _ => {}

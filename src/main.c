e9558c677f6f25c1db8e8421df25b8cc288dc40a
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2017-04-07 02:02:17 +0200

    Updated main for changes in the dispatcher & spinner

--- src/main.c
+++ src/main.c
@@ -1,40 +1,22 @@
 
-#include <stddef.h>
 #include <stdbool.h>
 
-#include <mouros/common.h>
-
-#include "worker.h"
+#include <mouros/tasks.h>
 
 #include "bsp.h"
-
 #include "message_dispatcher.h"
-#include "rust-bindings/spinner.h"
 
-__attribute__((aligned(8)))
-uint8_t spinner_comm_task_stack[TASK_STACK_SIZE];
-
-worker_t spinner_comm;
+#include "spinner/spinner.h"
 
 int main(void)
 {
 	os_init();
 
 	bsp_init();
-	msg_init();
-
-	void *spinner_ctx = spinner_get_context();
-	worker_task_init(&spinner_comm,
-	                 "spinner_comm",
-	                 spinner_comm_task_stack,
-	                 ARRAY_SIZE(spinner_comm_task_stack),
-	                 6,
-	                 spinner_comm_loop,
-	                 spinner_ctx);
-
-	worker_start(&spinner_comm);
 
 	dispatcher_init();
+
+	spinner_init();
 
 	os_tasks_start(1000);
 

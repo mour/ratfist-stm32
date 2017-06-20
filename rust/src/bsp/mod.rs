2fd69e5fa42a27c345da513ecb70493b70bc7ddc
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2017-06-21 01:17:20 +0200

    Added files forgotten in last commit

---
+++ rust/src/bsp/mod.rs
@@ -0,0 +1,6 @@
+
+#[cfg(feature = "stm32f411discovery")]
+mod stm32f411discovery;
+#[cfg(feature = "stm32f411discovery")]
+pub use self::stm32f411discovery::i2c;
+
---
+++ rust/src/bsp/stm32f411discovery.rs
@@ -0,0 +1,7 @@
+
+#[allow(dead_code)]
+pub mod i2c {
+    pub const I2C1: usize = 1073763328;
+    pub const I2C2: usize = 1073764352;
+    pub const I2C3: usize = 1073765376;
+}

49c0e3ec078256e598cf2096e378f1194c5ec96c
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2017-06-24 17:05:04 +0200

    More cleanup of I2C generic code
    Work on #18

--- rust/src/i2c/mod.rs
+++ rust/src/i2c/mod.rs
@@ -11,7 +11,7 @@ enum PeripheralState {
     AddressSent,
     Transmitting(usize),
     Receiving(usize),
-    Done(i32)
+    Done(i32),
 }
 
 
@@ -23,7 +23,8 @@ pub enum Step<'a> {
 impl<'a> AsMut<[u8]> for Step<'a> {
     fn as_mut(&mut self) -> &mut [u8] {
         match *self {
-            Step::Read(ref mut data) | Step::Write(ref mut data) => data,
+            Step::Read(ref mut data) |
+            Step::Write(ref mut data) => data,
         }
     }
 }
@@ -31,7 +32,8 @@ impl<'a> AsMut<[u8]> for Step<'a> {
 impl<'a> AsRef<[u8]> for Step<'a> {
     fn as_ref(&self) -> &[u8] {
         match *self {
-            Step::Read(ref data) | Step::Write(ref data) => data,
+            Step::Read(ref data) |
+            Step::Write(ref data) => data,
         }
     }
 }
@@ -40,7 +42,7 @@ impl<'a> Step<'a> {
     fn len(&self) -> usize {
         match *self {
             Step::Read(ref data) => data.len(),
-            Step::Write(ref data) => data.len()
+            Step::Write(ref data) => data.len(),
         }
     }
 }
@@ -49,7 +51,7 @@ impl<'a> Step<'a> {
 struct Transaction<'a, 'b: 'a> {
     steps: &'a mut [Step<'b>],
     curr_step: usize,
-    device_addr: u8
+    device_addr: u8,
 }
 
 impl<'a, 'b> Transaction<'a, 'b> {
@@ -58,45 +60,59 @@ impl<'a, 'b> Transaction<'a, 'b> {
     }
 }
 
-pub enum Peripheral {
-    I2C1
+pub enum Peripherals {
+    I2C1 = 0,
 }
 
-impl Into<usize> for Peripheral {
-    fn into(self) -> usize {
-        match self {
-            Peripheral::I2C1 => bsp::i2c::I2C1
+pub struct I2C<'a, 'b: 'a> {
+    it_context: &'a mut InterruptContext<'a, 'b>,
+}
+
+impl<'a, 'b> I2C<'a, 'b> {
+    pub fn new(periph: Peripherals) -> I2C<'a, 'b> {
+        I2C {
+            it_context: unsafe {
+                mem::transmute::<&mut InterruptContext, &mut InterruptContext>(
+                    &mut I2C_PERIPHS[periph as usize],
+                )
+            },
         }
     }
-}
 
-pub fn run_transaction<'a>(device_addr: u8, steps: &mut [Step<'a>]) -> bool {
-    unsafe {
-        critical!({
-            if IT_CONTEXT.current_transaction.is_some() || matches!(IT_CONTEXT.state, PeripheralState::Done(_)) {
-                return false;
-            }
-
-            let periph = i2c_v1::I2C::get_periph(IT_CONTEXT.periph_addr);
-            periph.reset();
-
-            IT_CONTEXT.current_transaction = Some(Transaction {
-                steps: mem::transmute::<&mut [Step], &mut [Step]>(steps),
-                curr_step: 0,
-                device_addr: device_addr
-            });
-        });
-
-        loop {
+    pub fn run_transaction(&mut self, device_addr: u8, steps: &mut [Step]) -> Result<(), ()> {
+        unsafe {
             critical!({
-                match IT_CONTEXT.state {
-                    PeripheralState::Done(ret_val) => {
-                        IT_CONTEXT.current_transaction = None;
-                        return ret_val == 0;
-                    },
-                    _ => {}
+                if self.it_context.current_transaction.is_some() ||
+                    matches!(self.it_context.state, PeripheralState::Done(_))
+                {
+                    return Err(());
                 }
+
+                let periph = i2c_v1::I2C::get_periph(self.it_context.periph_addr);
+                periph.reset();
+
+                self.it_context.current_transaction = Some(Transaction {
+                    steps: mem::transmute::<&mut [Step], &mut [Step]>(steps),
+                    curr_step: 0,
+                    device_addr: device_addr,
+                });
             });
+
+            loop {
+                critical!({
+                    match self.it_context.state {
+                        PeripheralState::Done(ret_val) => {
+                            self.it_context.current_transaction = None;
+                            if ret_val == 0 {
+                                return Ok(());
+                            } else {
+                                return Err(());
+                            }
+                        },
+                        _ => {}
+                    }
+                });
+            }
         }
     }
 }
@@ -105,12 +121,13 @@ pub fn run_transaction<'a>(device_addr: u8, steps: &mut [Step<'a>]) -> bool {
 pub struct InterruptContext<'a, 'b: 'a> {
     current_transaction: Option<Transaction<'a, 'b>>,
     periph_addr: usize,
-    state: PeripheralState
+    state: PeripheralState,
 }
 
-
-static mut IT_CONTEXT: InterruptContext = InterruptContext {
-    current_transaction: None,
-    periph_addr: bsp::i2c::I2C1,
-    state: PeripheralState::Done(0)
-};
+static mut I2C_PERIPHS: [InterruptContext; 1] = [
+    InterruptContext {
+        current_transaction: None,
+        periph_addr: bsp::i2c::I2C1,
+        state: PeripheralState::Done(0),
+    },
+];

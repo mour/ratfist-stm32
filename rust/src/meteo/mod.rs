31d6be43951837cdeb578997568fab704640b51b
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2017-06-24 18:36:33 +0200

    Initial do-nothing meteo module

--- rust/src/meteo/mod.rs
+++ rust/src/meteo/mod.rs
@@ -1,15 +1,19 @@
 
 use mouros_rust_bindings::CVoid;
 
-use i2c;
+use bsp::i2c;
 
 #[no_mangle]
 pub extern "C" fn meteo_comm_loop(_params: *mut CVoid) {
+    
+    let mut periph = i2c::I2C::new(i2c::Peripherals::I2C1);
     
     let mut write1 = [0u8; 10];    
     let mut read2 = [0u8; 4];
     
     let mut steps = [i2c::Step::Write(&mut write1), i2c::Step::Read(&mut read2)];
     
-    i2c::run_transaction(0xaa, &mut steps);
+    loop {
+        let _ = periph.run_transaction(0xaa, &mut steps);
+    }
 }

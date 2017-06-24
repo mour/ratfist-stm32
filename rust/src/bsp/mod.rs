6ff1566b82997be824c161269fa8bb1e3fe7f935
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2017-06-24 18:46:15 +0200

    Limited I2C to STM32F4 & added STM32F072 feature

--- rust/src/bsp/mod.rs
+++ rust/src/bsp/mod.rs
@@ -4,4 +4,6 @@ mod stm32f411discovery;
 #[cfg(feature = "stm32f411discovery")]
 pub use self::stm32f411discovery::i2c_periph_addr;
 
+// TODO to be removed once i2c_v2 is implemented
+#[cfg(feature = "stm32f411discovery")]
 pub mod i2c;
